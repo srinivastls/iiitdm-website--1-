908c2d1e63fb62e6565a6f7beade1be05cb593ee
sriram00620 <suryasriramamurthy2003@gmail.com>
2025-02-22 21:56:23 +0530

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata: Metadata = {
   title: 'IIITDM CSE Department - Home', 
@@ -14,6 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>{children}</body>
+      <Analytics/>
     </html>
   )
 }
